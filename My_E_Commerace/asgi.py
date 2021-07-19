35e646b3fed44e1f31cd644776ad6d52e7d4fdbf
Arslan-bit <arslan9.r@gmail.com>
2021-07-20 02:28:41 +0500

    Initial commit

---
+++ My_E_Commerace/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for My_E_Commerace project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'My_E_Commerace.settings')
+
+application = get_asgi_application()
